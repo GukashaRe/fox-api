5e315b13e39f8003d533168bf007e3cf817e626f
WFGukasha <gukasha@163.com>
2026-06-15 15:19:22 +0800

    加了一些数据库基本功能

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 pub mod server_config;
 
-use actix_web::web::Json;
+use actix_web::error::ErrorInternalServerError;
+use actix_web::web::{Data, Json};
 use actix_web::{App, HttpServer, Result as ActixResult, post};
 use anyhow::Result;
 use serde_json::Value;
+use sqlx::{PgPool, Row, query};
 
 #[post("/post_json")]
-async fn post_json(data: Json<Value>) -> ActixResult<String> {
-    Ok(data.to_string())
+async fn post_json(_data: Json<Value>, base: Data<PgPool>) -> ActixResult<String> {
+    let res = query("SELECT reason FROM group_blacklist gb WHERE qq = $1")
+        .bind(1000)
+        .fetch_one(base.as_ref())
+        .await
+        .map_err(|e| {
+            eprintln!("数据库查询失败 {}", e);
+            ErrorInternalServerError("database err")
+        })?;
+    Ok(res.get("reason"))
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = server_config::check_or_crate_config().await?;
-    Ok(HttpServer::new(|| App::new().service(post_json))
-        .bind(("127.0.0.1", 10013))?
-        .run()
-        .await?)
+    let database = PgPool::connect(&config.pgsql_url).await?;
+    Ok(
+        HttpServer::new(move || App::new().service(post_json).app_data(database.clone()))
+            .bind(("127.0.0.1", 10013))?
+            .run()
+            .await?,
+    )
 }
