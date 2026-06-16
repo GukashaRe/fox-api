c87dbdd650ada8e491c07c59d02fca287659d607
WFGukasha <gukasha@163.com>
2026-06-16 13:38:26 +0800

    自定义logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+pub mod fox_logger;
 pub mod server_config;
 
+// 重导出关键的函数，这样 $crate::get_logger() 就能找到了
+use crate::fox_logger::{LogLevel, Logger, init_logger};
 use actix_web::error::ErrorInternalServerError;
 use actix_web::web::{Data, Json};
 use actix_web::{App, HttpResponse, HttpServer, Responder, Result as ActixResult, post};
 use anyhow::Result;
 use env_logger::Env;
+pub use fox_logger::get_logger;
+pub use fox_logger::init_default;
+pub use fox_logger::init_with_file;
 use serde_json::Value;
 use sqlx::{PgPool, Row, query};
 
@@ -15,7 +21,7 @@ async fn post_json(_data: Json<Value>, base: Data<PgPool>) -> ActixResult<impl R
         .fetch_one(base.as_ref())
         .await
         .map_err(|e| {
-            println!("{}:{} 数据库查询失败 {}", file!(), column!(), e);
+            log_error!("数据库查询失败 {}", e);
             ErrorInternalServerError("database err")
         })?;
     let s: String = res.get("reason");
@@ -24,6 +30,9 @@ async fn post_json(_data: Json<Value>, base: Data<PgPool>) -> ActixResult<impl R
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let mut logger = Logger::new();
+    logger.set_min_level(LogLevel::Debug);
+    init_logger(logger).unwrap();
     env_logger::init_from_env(Env::default().default_filter_or("debug"));
     let config = server_config::check_or_crate_config().await?;
     let database = PgPool::connect(&config.pgsql_url).await?;
